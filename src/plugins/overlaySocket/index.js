346f0859c544390f08030768e07568cdd0456f58
Jaid <jaid.jsx@gmail.com>
2019-12-04 07:24:17 +0100

    feat: Forwarding `updateChatters` to overlay
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/overlaySocket/index.js
+++ src/plugins/overlaySocket/index.js
@@ -2,16 +2,30 @@ import socketIo from "socket.io"
 
 import socketEnhancer from "lib/socketEnhancer"
 
-import core, {logger} from "src/core"
+import {logger} from "src/core"
 
 class SocketClient {
 
+  setCoreReference(core) {
+    this.core = core
+  }
+
   init() {
-    this.socketServer = socketIo(core.insecureServer)
+    this.socketServer = socketIo(this.core.insecureServer)
     this.socketServer.on("connection", client => {
       logger.info("%s connected", client.id)
     })
     socketEnhancer.enhanceServer(this.socketServer)
+  }
+
+  postInit() {
+    this.socket = this.core.plugins.socketClient.socket
+  }
+
+  ready() {
+    this.socket.on("updateChatters", async chatters => {
+      this.socketServer.emit("updateChatters", chatters)
+    })
   }
 
 }
